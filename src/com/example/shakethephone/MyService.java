7beb8174cc1e936bb36506f4df8a0503813511be
Dionysios <dpapathanopoulos@webst.gr>
2014-02-23 14:04:23 +0100

    first version of managing volume

--- src/com/example/shakethephone/MyService.java
+++ src/com/example/shakethephone/MyService.java
@@ -2,7 +2,10 @@ package com.example.shakethephone;
 
 import android.app.Service;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.os.IBinder;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Toast;
 
 public class MyService extends Service {
@@ -17,6 +20,8 @@ public class MyService extends Service {
 	   public int onStartCommand(Intent intent, int flags, int startId) {
 	      // Let it continue running until it is stopped.
 	      Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+	      
+	    
 	      return START_STICKY;
 	   }
 	   @Override
